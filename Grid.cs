d262f0a84aecb8b1ac6e45b23732f3e74f64e2d3
Sergey Kumkov <sskumk@gmail.com>
2018-12-03 15:13:57 +0500

    Посидели днем в понедельник 03.12.

--- Grid.cs
+++ Grid.cs
@@ -1,22 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 
 namespace CouchyProblem
-    
 {
+    public class GridStorage : SortedDictionary<PhasePoint, Dictionary<double, double>>
+    {
+    }
+
     public class Grid
     {
-        public static double Step = 0.1;
+        public PhasePoint Steps { get; protected set; }
 
-        static SortedDictionary<PhasePoint, Dictionary<double, double>> grid = new SortedDictionary<PhasePoint, Dictionary<double, double>>();
-
-        public static bool HasNeighbour(PhasePoint p1, int dir)
+//        static SortedDictionary<PhasePoint, Dictionary<double, double>> grid = 
+//            new SortedDictionary<PhasePoint, Dictionary<double, double>>();
+        readonly GridStorage grid = new GridStorage();
+        
+        // Фабрики
+        
+        // Сетка, накинутая на прямоугольный параллелепипед
+        public static Grid BoxGrid(PhasePoint ld, PhasePoint sizes, PhasePoint steps)
         {
+            return null;
+        }
+
+        // Сетка, накинутая на шар
+        public static Grid BallGrid(PhasePoint center, double radius, PhasePoint steps)
+        {
+            return null;
+        }
+        
+        // Может быть, еще что-то...
+        
+        public bool HasNeighbour(PhasePoint p1, int dir)
+        {
+            int absDir = Math.Abs(dir);
             PhasePoint p3 = new PhasePoint(p1);
-            p3.coords[dir] += Step;
+            p3.coords[absDir] += Steps[absDir] * Math.Sign(dir);
             return grid.ContainsKey(p3);
         }
 
