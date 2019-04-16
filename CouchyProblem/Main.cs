e90c001764415c70565dca51a7a5cf1fff4c4ead
oneza <fedor-menshikov@yandex.ru>
2019-04-16 09:23:30 +0500

    ctrl+c ctrl+v для ControlConstraints

--- CouchyProblem/Main.cs
+++ CouchyProblem/Main.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Contracts;
 
 namespace CouchyProblem
@@ -17,13 +16,7 @@ namespace CouchyProblem
             //    steps = new PhasePoint(new List<double> { 1, 1, 1 }),
             //    ld = new PhasePoint(new List<double> { 1, 1, 1 });
             //Grid grid = Grid.BoxGrid(ld, sizes, steps);
-            PhasePoint
-                center = new PhasePoint(new List<double> { 0, 0, 0 }),
-                steps = new PhasePoint(new List<double> { 1, 1, 1 });
-            double radius = 2;
-            Grid grid = Grid.BallGrid(center, radius, steps);
-            Console.WriteLine(grid);
-            
+
             LibLinking<IDynamics> loader = new LibLinking<IDynamics>();
             IDynamics dynam = loader.FindLib("./bin/Debug/netcoreapp2.1/SimpleMotion.dll");
 
@@ -32,7 +25,17 @@ namespace CouchyProblem
                 v = new PhasePoint(new List<double> {-2, 1}),
                 speed = dynam.f(0, new PhasePoint(), u, v);
             Console.WriteLine(speed);
+
+            PhasePoint
+                center = new PhasePoint(new List<double> {0, 0}),
+                steps = new PhasePoint(new List<double> {0.25, 0.25});
+            double radiusP = 1;
+            double radiusQ = 0.5;
+            ControlConstraints P = ControlConstraints.BallConstraints(center, radiusP, steps);
+            ControlConstraints Q = ControlConstraints.BallConstraints(center, radiusQ, steps);
+            List<double> time = new List<double> {0, 1, 2, 3, 4, 5};
             
+
             Console.ReadKey();
         }
     }
