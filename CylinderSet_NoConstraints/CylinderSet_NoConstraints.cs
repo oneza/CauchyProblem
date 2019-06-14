3985f9318be6b16bbd8b2c49100b21eaec22ef13
Sergey Kumkov <sskumk@gmail.com>
2019-06-14 15:42:29 +0500

    Функции хи и тета для просчитанного примера

---
+++ CylinderSet_NoConstraints/CylinderSet_NoConstraints.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+using Contracts;
+
+namespace CylinderSet_NoConstraints
+{
+  public class CylinderSet_NoConstraints
+  {
+    public class Sets : IChi, ITheta
+    {
+      private double r;
+      
+      public Sets(double finalR)
+      {
+        r = finalR;
+      }
+      
+      public double theta(double t, PhasePoint x)
+      {
+        return 0;
+      }
+
+      public double chi(double t, PhasePoint x)
+      {
+        double norm = Math.Sqrt(x
+                                .Select(a => a * a)
+                                .Sum());
+        if (norm > r)
+        {
+          return (norm / r) - 1;
+        }
+        else return 0;
+      }
+    }    
+  }
+}
