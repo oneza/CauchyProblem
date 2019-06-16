b85b962aea288399dec4cc60d042962d36ef72b5
oneza <fedor-menshikov@yandex.ru>
2019-06-16 14:03:02 +0500

     

--- CouchyProblem/Main.cs
+++ CouchyProblem/Main.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using Contracts;
+using System.IO;
 
 namespace CouchyProblem
 {
@@ -13,22 +14,33 @@ namespace CouchyProblem
   {
     static public void Main(string[] args)
     {
-      //PhasePoint
-      //    sizes = new PhasePoint(new List<double> { 3, 3, 3 }),
-      //    steps = new PhasePoint(new List<double> { 1, 1, 1 }),
-      //    ld = new PhasePoint(new List<double> { 1, 1, 1 });
-      //Grid grid = Grid.BoxGrid(ld, sizes, steps);
-
+      
+      double t1 = 0;
+      double T = 5;
+      double delta = 0.1;
+      int nT = (int)Math.Ceiling((T - t1) / delta);
+      double[] time = new double[nT+1];
+      for (int i = 0; i <= nT; i++)
+      {
+        time[nT - i] = T - i * delta;
+      }
+      
       string DLLPath = "./bin/Debug/netcoreapp2.1/";
+      
       LibLinking<IDynamics> loader = new LibLinking<IDynamics>();
       IDynamics dynam = loader.FindLib(DLLPath + "SimpleMotion.dll");
       
-//      LibLinking<ISigma> sigmaloader = new LibLinking<ISigma>();
-//      ISigma sigma = sigmaloader.FindLib(DLLPath + "Sigma.dll");
-      
       double finalR = 1.0;
+      double finalT = T;
+      
       LibLinking<IChi> chiloader = new LibLinking<IChi>();
       IChi chi = chiloader.FindLib(DLLPath + "CylinderSet_NoConstraints.dll", finalR);
+      
+      LibLinking<IChi> chi1loader = new LibLinking<IChi>();
+      IChi chi1 = chi1loader.FindLib(DLLPath + "CylinderSet.dll", finalR, finalT);
+      
+      string inPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+      string outPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
 //            ControlConstraints cc =
 //                ControlConstraints.BallConstraints(new PhasePoint(2), 1,
@@ -41,6 +53,15 @@ namespace CouchyProblem
 //                v = new PhasePoint(new List<double> {-2, 1}),
 //                speed = dynam.f(0, new PhasePoint(), u, v);
 //            Console.WriteLine(speed);
+      List<string> inArgs = new List<string>();
+      using (StreamReader sr = new StreamReader(Path.Combine(inPath, "In.txt")))
+      {
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+          inArgs.Add(line);
+        }
+      }
 
       PhasePoint
         center = new PhasePoint(new List<double> {0, 0}),
@@ -55,27 +76,30 @@ namespace CouchyProblem
       ControlConstraints Q = ControlConstraints.BallConstraints(center, radiusQ, qSteps);
       List<PhasePoint> res = new List<PhasePoint>();
       
-      double t1 = 0;
-      double T = 5;
-      double delta = 0.1;
-      int nT = (int)Math.Ceiling((T - t1) / delta);
-      double[] time = new double[nT+1];
-      for (int i = 0; i <= nT; i++)
-      {
-        time[nT - i] = T - i * delta;
-      }
-      
       //Grid grid = Grid.BallGrid(center, 3, steps);
       foreach (PhasePoint x in grid.Keys.ToList())
-        grid[x] = new SortedDictionary<double, double> {{T, chi.chi(T, x)}};
+        grid[x] = new SortedDictionary<double, double> {{T, chi1.chi(T, x)}};
       
       for (int i = time.Length - 1; i > 0; i--)
       {
         foreach (PhasePoint x in grid.Keys)
         {
           double velocity = Minmax(dynam, time[i], x, P, Q, grid);
-          grid[x].Add(time[i-1], Math.Min(chi.chi(time[i-1], x), 
+          grid[x].Add(time[i-1], Math.Min(chi1.chi(time[i-1], x), 
             grid[x][time[i]] + (time[i] - time[i-1]) * velocity));
+        }
+      }
+
+      using (StreamWriter outputFile = new StreamWriter(Path.Combine(outPath, "Result2.txt")))
+      {
+        int i = 1;
+        foreach (PhasePoint x in grid.Keys)
+        {
+          foreach (double t in grid[x].Keys)
+          {
+            outputFile.WriteLine(i+") "+"Coordinates:" + " " + x + " " + "Time:" + " " + t + " " + "Result:" + " "+ Math.Round(grid[x][t], 8));
+            i++;
+          }
         }
       }
 
---
+++ CylinderSet/CylinderSet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Contracts;
+using System.Linq;
+
+namespace CylinderSet
+{
+    public class CylinderSet
+    {
+        public class Sets : IChi, ITheta
+        {
+            private double r;
+            private double T;
+      
+            public Sets(double finalR,double finalT)
+            {
+                r = finalR;
+                T = finalT;
+            }
+      
+            public double theta(double t, PhasePoint x)
+            {
+                return 0;
+            }
+
+            public double chi(double t, PhasePoint x)
+            {
+                double norm = Math.Sqrt(x
+                    .Select(a => a * a)
+                    .Sum());
+                if (norm > r)
+                {
+                    return (norm / r) - 1 - t + T;
+                }
+                else return T - t;
+            }
+        }    
+    }
+}
