12e84d79fbd92434940f9aefbcfe831565d4a04e
oneza <fedor-menshikov@yandex.ru>
2019-07-07 23:55:42 +0500

     

--- CouchyProblem/Main.cs
+++ CouchyProblem/Main.cs
@@ -17,8 +17,18 @@ namespace CouchyProblem
       string DLLPath = "./bin/Debug/netcoreapp2.1/";
       string inPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
       string outPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-      
-      List<string> inArgs = new List<string>();
+
+      double t1 = 0;
+      double T = 5;
+      double delta = 0.025;
+      int nT = (int) Math.Ceiling((T - t1) / delta);
+      double[] time = new double[nT + 1];
+      for (int i = 0; i <= nT; i++)
+      {
+        time[nT - i] = T - i * delta;
+      }
+
+/*      List<string> inArgs = new List<string>();
       using (StreamReader sr = new StreamReader(Path.Combine(inPath, "In.txt")))
       {
         string line;
@@ -61,19 +71,19 @@ namespace CouchyProblem
       {
         ControlConstraints Q = ControlConstraints.BallConstraints(center, radiusQ, qSteps);
       }
-      
+*/
       LibLinking<IDynamics> loader = new LibLinking<IDynamics>();
       IDynamics dynam = loader.FindLib(DLLPath + "SimpleMotion.dll");
-      
+
       double finalR = 1.0;
       double finalT = T;
-      
-      LibLinking<IChi> chi1loader = new LibLinking<IChi>();
-      IChi chi1 = chi1loader.FindLib(DLLPath + "CylinderSet.dll", finalR, finalT);
-      
-      LibLinking<ITheta> theta1loader = new LibLinking<ITheta>();
-      ITheta theta1 = theta1loader.FindLib(DLLPath + "CylinderSet.dll", finalR, finalT);
-      
+
+//      LibLinking<IChi> chi1loader = new LibLinking<IChi>();
+//      IChi chi1 = chi1loader.FindLib(DLLPath + "CylinderSet.dll", finalR, finalT);
+
+//      LibLinking<ITheta> theta1loader = new LibLinking<ITheta>();
+//      ITheta theta1 = theta1loader.FindLib(DLLPath + "CylinderSet.dll", finalR, finalT);
+
 /*      double t1 = 0;
       double T = 5;
       double delta = 0.1;
@@ -83,45 +93,69 @@ namespace CouchyProblem
       {
         time[nT - i] = T - i * delta;
       }
-      
+*/
       LibLinking<IChi> chiloader = new LibLinking<IChi>();
       IChi chi = chiloader.FindLib(DLLPath + "CylinderSet_NoConstraints.dll", finalR);
-      
+
       LibLinking<ITheta> thetaloader = new LibLinking<ITheta>();
-      ITheta theta = thetaloader.FindLib(DLLPath + "CylinderSet_NoConstraints.dll");
+      ITheta theta = thetaloader.FindLib(DLLPath + "CylinderSet_NoConstraints.dll", finalR);
 
-            ControlConstraints cc =
-                ControlConstraints.BallConstraints(new PhasePoint(2), 1,
+      /*          ControlConstraints cc =
+                    ControlConstraints.BallConstraints(new PhasePoint(2), 1,
+                        new PhasePoint(new List<double> {0.1, 0.1}));
+                Grid g = Grid.BallGrid(new PhasePoint(2), 1,
                     new PhasePoint(new List<double> {0.1, 0.1}));
-            Grid g = Grid.BallGrid(new PhasePoint(2), 1,
-                new PhasePoint(new List<double> {0.1, 0.1}));
+    
+                PhasePoint
+                    u = new PhasePoint(new List<double> {1, 0}),
+                    v = new PhasePoint(new List<double> {-2, 1}),
+                    speed = dynam.f(0, new PhasePoint(), u, v);
+                Console.WriteLine(speed);      
+                */
 
-            PhasePoint
-                u = new PhasePoint(new List<double> {1, 0}),
-                v = new PhasePoint(new List<double> {-2, 1}),
-                speed = dynam.f(0, new PhasePoint(), u, v);
-            Console.WriteLine(speed);      
-      Grid grid = Grid.BallGrid(center, 4.6, xSteps);
+      PhasePoint
+        center = new PhasePoint(new List<double> {0, 0}),
+        xSteps = new PhasePoint(new List<double> {0.1, 0.1}),
+        pSteps = xSteps,
+        qSteps = new PhasePoint(new List<double> {0.1, 0.1});
+      double radiusX = 4.6;
+      double radiusP = 1;
+      double radiusQ = 0.5;
+      Grid grid = Grid.BallGrid(center, radiusX, xSteps);
       ControlConstraints P = ControlConstraints.BallConstraints(center, radiusP, pSteps);
-      ControlConstraints Q = ControlConstraints.BallConstraints(center, radiusQ, qSteps);*/
-      
+      ControlConstraints Q = ControlConstraints.BallConstraints(center, radiusQ, qSteps);
+
       foreach (PhasePoint x in grid.Keys.ToList())
-        grid[x] = new SortedDictionary<double, double> {{T, chi1.chi(T, x)}};
+        grid[x] = new SortedDictionary<double, double> {{T, chi.chi(T, x)}};
+
+      int length = time.Length;
       
-      for (int i = time.Length - 1; i > 0; i--)
+      Parallel.For(0, length  , i =>
       {
         foreach (PhasePoint x in grid.Keys)
         {
-          double velocity = Minmax(dynam, time[i], x, P, Q, grid);
-          grid[x].Add(time[i-1], Math.Max(Math.Min(chi1.chi(time[i-1], x), 
-            grid[x][time[i]] + (time[i] - time[i-1]) * velocity), 
-            theta1.theta(time[i-1], x)));
+          double velocity = Minmax(dynam, time[length - i], x, P, Q, grid);
+          grid[x].Add(time[length - i - 1], Math.Max(Math.Min(chi.chi(time[length - i - 1], x),
+              grid[x][time[length - i]] + (time[length - i] - time[length - i - 1]) * velocity),
+            theta.theta(time[length - i - 1], x)));
         }
-      }
+      });
 
-      using (StreamWriter outputFile = new StreamWriter(Path.Combine(outPath, "Result3.txt")))
+
+/*      for (int i = time.Length - 1; i > 0; i--)
       {
-        int i = 1;
+        foreach(PhasePoint x in grid.Keys)
+        {
+          double velocity = Minmax(dynam, time[i], x, P, Q, grid);
+          grid[x].Add(time[i-1], Math.Max(Math.Min(chi.chi(time[i-1], x), 
+            grid[x][time[i]] + (time[i] - time[i-1]) * velocity), 
+            theta.theta(time[i-1], x)));
+        }
+      }*/
+
+      using (StreamWriter outputFile = new StreamWriter(Path.Combine(outPath, "Result6.txt")))
+      {
+//        int i = 1;
         foreach (PhasePoint x in grid.Keys)
         {
           foreach (double t in grid[x].Keys)
@@ -129,7 +163,7 @@ namespace CouchyProblem
             System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
             //outputFile.WriteLine(i+") "+"Coordinates:" + " " + x + " " + "Time:" + " " + t + " " + "Result:" + " "+ Math.Round(grid[x][t], 8));
             outputFile.WriteLine(x.First() + " " + x.Last() +  " " + t + " " + Math.Round(grid[x][t], 8));
-            i++;
+//            i++;
           }
         }
       }
